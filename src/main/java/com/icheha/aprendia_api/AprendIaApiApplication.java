432bb1ea4056ac9bffa0344844f43f9d36d80eb2
HexagonalSole1 <deskrun@gmail.com>
2025-09-10 11:45:03 -0600

    first commit

---
+++ src/main/java/com/icheha/aprendia_api/AprendIaApiApplication.java
@@ -0,0 +1,13 @@
+package com.icheha.aprendia_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AprendIaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AprendIaApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/icheha/aprendia_api/AprendIaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.icheha.aprendia_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AprendIaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
