5ed4620b5638e5f58f528db4dd8f765d44963060
Alexis Guzman <spartanthelexvro@gmail.com>
2025-10-18 14:53:43 -0600

    ¨Configurando para embedding

--- src/main/java/com/icheha/aprendia_api/assets/assets/data/entities/AssetEntity.java
+++ src/main/java/com/icheha/aprendia_api/assets/assets/data/entities/AssetEntity.java
@@ -1,10 +1,12 @@
 package com.icheha.aprendia_api.assets.assets.data.entities;
 
+import io.hypersistence.utils.hibernate.type.array.FloatArrayType;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.hibernate.annotations.JdbcTypeCode;
+import org.hibernate.annotations.Type;
 import org.hibernate.type.SqlTypes;
 
 @Entity
@@ -27,7 +29,7 @@ public class AssetEntity {
     @Column(name = "descripcion", nullable = false)
     private String description;
 
-    @JdbcTypeCode(SqlTypes.JSON)
-    @Column(name = "vector", nullable = false)
+    @Type(FloatArrayType.class)
+    @Column(name = "vector", nullable = false, columnDefinition = "vector(384)")
     private float[] vector;
 }
